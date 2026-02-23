deb4cee46ffc91f3bfdfed1e35669b1e515c64e9
Keith Shark <kshark.it@csi.edu>
2026-02-23 15:29:29 -0700

    Minor bug fixes & updates
    Updates:
    
    if displayName does exist ignore attempts to edit displayName
    
    remove leading and trailing characters provided for newName
    
    some log messages were not printing on new lines
    
    Add:
    
    raise error the terminate program at any state
    
    log if an asset has no relations
    
    README instructions

--- renaming_script.py
+++ renaming_script.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 from warnings import warn
 from datetime import datetime
 
+#TODO: add file extension checks.
+
 #logging
 log_file = open(f'./{datetime.now().isoformat()}.log', 'w', buffering=1)
 
@@ -77,6 +79,9 @@ try:
                     print(f"Getting {img_path} ")
 
                     parent_folder_path = img_path[:img_path.rindex("/")]
+                    
+                    new_name += img_path[img_path.rfind('.'):]
+
                     new_image_path = f"{parent_folder_path}/{new_name}"
 
                     resp = session.get(
@@ -89,9 +94,8 @@ try:
                         continue
                     asset = strip_cascade_object(data, asset_type)
                     
-                    if "metadata" in asset and "dynamicFields" in asset["metadata"]:
-                        
-                        asset.update(displayName=new_name)
+                    if "metadata" in asset and "displayName" in asset["metadata"]:
+                        asset['metadata']['displayName'] = new_name
                         asset_payload = json.dumps({"asset":{asset_type : asset}})
                         
                         # rename the image before moving
@@ -100,101 +104,104 @@ try:
                                 headers=header,
                                 data=asset_payload
                             )
-                        
                         edit_status = edit_response.json()
                         if "success" in edit_status and not edit_status["success"]:
                             log_file.write(f"Error: {img_path} unsuccessful at updating. Return message:{edit_status['message']}\n")
                         else:
                             log_file.write(f"{img_path} renamed display name to {new_name}\n")
-                        
-
-                        move_parameters = {
-                            "moveParameters": {
-                                "destinationContainerIdentifier": {
-                                "path": {
-                                    "path": parent_folder_path,
-                                    "siteName": sitename
-                                },
-                                "type": "folder"
-                                },
-                                "doWorkflow": False,
-                                "newName": new_name,
-                                "unpublish": True
-                            }
-                        }
-
-                        # move the file/renaming it
-                        move_response = session.post(
-                                f"{base_url}/move/{asset_type}/{sitename}/{img_path}",
-                                headers=header,
-                                json=move_parameters
-                            )
-                        
-                        move_status = move_response.json()
-                        if "success" in move_status and not move_status["success"]:
-                            log_file.write(f"Error: unsuccessful at moving {img_path}. Return message:{move_status['message']}\n")
-                        else:
-                            log_file.write(f"Moved {img_path} and renamed to {new_name}\n")
-
-                        # publish the image first
-                        img_pub_response = session.post(
-                                f"{base_url}/publish/{asset_type}/{sitename}/{new_image_path}",
-                                headers=header,
-                            )
-                        
-                        img_pub_status = img_pub_response.json()
-                        if "success" in img_pub_status and not img_pub_status["success"]:
-                            log_file.write(f"Error: unsuccessful publish {new_image_path}. Return message:{img_pub_status['message']}\n")
-                        else:
-                            log_file.write(f"Published {new_image_path}\n")
-
-                        # pull relationships for the image
-                        list_sub_response = session.get(
-                                f"{base_url}/listSubscribers/{asset_type}/{sitename}/{new_image_path}",
-                                headers=header,
-                            )
-                        
-                        relationships_result = list_sub_response.json()
-                        
-                        if "success" in relationships_result and not relationships_result["success"]:
-                            log_file.write(f"Error: unsuccessful getting relationships for {new_image_path}. Return message:{relationships_result['message']}\n")
-                        
-
-                        log_file.write(f"Publishing related asset to {new_image_path}")
-                        print(f"{img_path} relationships:")
-                        for relation_object in relationships_result["subscribers"]:
-                            asset_path = relation_object['path']['path']
-                            r_asset_type = relation_object['type'] # prevent conflict with the asset_type used for images
-                            
-                            # Make the the asset is publishable
-                            is_publish_response = session.get(
-                                f"{base_url}/read/{r_asset_type}/{sitename}/{asset_path}",
-                                headers=header
-                            )
-                            
-                            is_publish_status = strip_cascade_object(is_publish_response.json(), r_asset_type)
-                            if not is_publish_status.get("shouldBePublished"):
-                                log_file.write(f"*{asset_path} Skipped: shouldBePublished to False")
-                                continue
-
-                            print(asset_path)
-                            # publish each related asset
-                            publish_response = session.post(
-                                f"{base_url}/publish/{r_asset_type}/{sitename}/{asset_path}",
-                                headers=header,
-                            )
-                            publish_status = publish_response.json()
-                            if "success" in publish_status and not publish_status["success"]:
-                                log_file.write(f"Error: unsuccessful at publishing {asset_path}. Return message:{publish_status['message']}\n")
-                            else:
-                                log_file.write(f">> Published {asset_path}\n")
-                        
                     else:
-                        raise RuntimeError("asset does not have metadata")
+                        log_file.write(f"Warn: Skip renaming {img_path} DisplayName, displayName metadata does not exist\n")
 
+                    move_parameters = {
+                        "moveParameters": {
+                            "destinationContainerIdentifier": {
+                            "path": {
+                                "path": parent_folder_path,
+                                "siteName": sitename
+                            },
+                            "type": "folder"
+                            },
+                            "doWorkflow": False,
+                            "newName": new_name,
+                            "unpublish": True
+                        }
+                    }
+
+                    # move the file/renaming it
+                    move_response = session.post(
+                            f"{base_url}/move/{asset_type}/{sitename}/{img_path}",
+                            headers=header,
+                            json=move_parameters
+                        )
+                    
+                    move_status = move_response.json()
+                    if "success" in move_status and not move_status["success"]:
+                        log_file.write(f"Error: unsuccessful at moving {img_path}. Return message:{move_status['message']}\n")
+                        raise RuntimeError('** OPERATION UNSUCCESSFUL **\nRefer to log file for details')
+                    else:
+                        log_file.write(f"Moved {img_path} and renamed to {new_name}\n")
+
+                    # publish the image first
+                    img_pub_response = session.post(
+                            f"{base_url}/publish/{asset_type}/{sitename}/{new_image_path}",
+                            headers=header,
+                        )
+                    
+                    img_pub_status = img_pub_response.json()
+                    if "success" in img_pub_status and not img_pub_status["success"]:
+                        log_file.write(f"Error: unsuccessful publish {new_image_path}. Return message:{img_pub_status['message']}\n")
+                        raise RuntimeError('** OPERATION UNSUCCESSFUL **\nRefer to log file for details')
+                    else:
+                        log_file.write(f"Published {new_image_path}\n")
+
+                    # pull relationships for the image
+                    list_sub_response = session.get(
+                            f"{base_url}/listSubscribers/{asset_type}/{sitename}/{new_image_path}",
+                            headers=header,
+                        )
+                    
+                    relationships_result = list_sub_response.json()
+                    
+                    if "success" in relationships_result and not relationships_result["success"]:
+                        log_file.write(f"Error: unsuccessful getting relationships for {new_image_path}. Return message:{relationships_result['message']}\n")
+                        raise RuntimeError('** OPERATION UNSUCCESSFUL **\nRefer to log file for details')
                     
 
+                    log_file.write(f"Publishing related asset to {new_image_path}\n")
+                    print(f"{img_path} relationships:")
+                    
+                    if len(relationships_result["subscribers"]) == 0:
+                        log_file.write(f">> No other assets associated")
+                        continue
+                    
+                    for relation_object in relationships_result["subscribers"]:
+                        asset_path = relation_object['path']['path']
+                        r_asset_type = relation_object['type'] # prevent naming conflict with the asset_type used for images
                         
+                        # Make the the asset is publishable
+                        is_publish_response = session.get(
+                            f"{base_url}/read/{r_asset_type}/{sitename}/{asset_path}",
+                            headers=header
+                        )
+                        
+                        is_publish_status = strip_cascade_object(is_publish_response.json(), r_asset_type)
+                        if not is_publish_status.get("shouldBePublished"):
+                            log_file.write(f"*{asset_path} Skipped: shouldBePublished to False\n")
+                            continue
+
+                        print(asset_path)
+                        # publish each related asset
+                        publish_response = session.post(
+                            f"{base_url}/publish/{r_asset_type}/{sitename}/{asset_path}",
+                            headers=header,
+                        )
+                        publish_status = publish_response.json()
+                        if "success" in publish_status and not publish_status["success"]:
+                            log_file.write(f"Error: unsuccessful at publishing {asset_path}. Return message:{publish_status['message']}\n")
+                            raise RuntimeError('** OPERATION UNSUCCESSFUL **\nRefer to log file for details')
+                        else:
+                            log_file.write(f">> Published {asset_path}\n")
+                
                 except requests.JSONDecodeError:
                     print(f"Request did not return a valid JSON format. (Most likely a HTML response)")
                 except requests.RequestException:
